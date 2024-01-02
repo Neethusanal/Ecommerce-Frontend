96c0257d12648a38e5eb56eafeb9ea1e278bdfbc
Neethusanal <neethukumarkk@gmail.com>
2024-01-02 13:33:56 +0530

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+  
+export const Header= ()=>{
+return(
+    <div className='header'>
+<Link to ='/'><span>Home</span> </Link>
+<Link to ='/products'><span>Products</span>  </Link>
+<Link to ='/blog'><span>Blog</span> </Link>
+<Link to ='/aboutus'><span>About Us</span>  </Link>
+    </div>
+)
+}
--- src/Router/UserRoute.jsx
+++ src/Router/UserRoute.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Login from '../Components/Login'
 import Home from '../Components/Home'
+import { Header } from '../Components/Header'
 
 const UserRoute = () => {
   return (
     <div>
-      
+      <div>
+        <Header/>
+      </div>
      
       <Routes>
+       
             <Route exact path="/login" element={<Login/>}/>
             <Route exact path="/" element={<Home/>}/>
         </Routes>
