2d8e9f9e3b7aa2716fa433eeaebae28872dd18a2
Neethusanal <neethukumarkk@gmail.com>
2024-01-02 09:13:46 +0530

    home

--- src/Router/UserRoute.jsx
+++ src/Router/UserRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Login from '../Components/Login'
+import Home from '../Components/Home'
 
 const UserRoute = () => {
   return (
@@ -9,6 +10,7 @@ const UserRoute = () => {
      
       <Routes>
             <Route exact path="/login" element={<Login/>}/>
+            <Route exact path="/" element={<Home/>}/>
         </Routes>
         
         
