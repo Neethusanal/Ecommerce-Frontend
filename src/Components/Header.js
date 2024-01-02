af55966be8dd4731004e58b87fb512bf823f863a
Neethusanal <neethukumarkk@gmail.com>
2024-01-02 17:53:30 +0530

    new

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,7 @@ return(
     <div className='header'>
 <Link to ='/'><span>Home</span> </Link>
 <Link to ='/products'><span>Products</span>  </Link>
+<Link to ='/details'><span>ProductDetails</span> </Link>
 <Link to ='/blog'><span>Blog</span> </Link>
 <Link to ='/aboutus'><span>About Us</span>  </Link>
     </div>
